dd605f57fa23fd4c732df9a29ece9cf09349cc8f
Jayraj Singh Solanki <jayrajsinghsolanki2004@gmail.com>
2026-04-08 12:58:20 +0530

    Add registration path to urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('update/<int:id>/', views.update_products, name='update_products'), #update_product 
     path('', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
