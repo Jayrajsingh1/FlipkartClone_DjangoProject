29005cd20305a47fc0ad6cb15c9098ef72574bc8
Jayraj Singh Solanki <jayrajsinghsolanki2004@gmail.com>
2026-04-08 11:43:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FlipkartClone_DjangoProject
-Flipkart Clone built with Django featuring product management (CRUD), image upload, and responsive UI.
+# Django_CRUDProject
---
+++ products/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Product
+
+# Create your views here.
+
+@login_required
+def index(request):
+    return render(request,'index.html')
+
+
+@login_required
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+# add_product start
+
+@login_required
+def add_product(request):
+    if request.method == "POST":
+        id = request.POST.get('ID')
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        brand = request.POST.get('brand')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        image = request.FILES.get('image')
+        pdfs = request.FILES.get('pdfs')
+
+        Product.objects.create(
+            ID=id,
+            name=name,
+            category=category,
+            brand=brand,
+            description=description,
+            price=price,
+            image=image,
+            pdfs=pdfs
+        )
+
+        return redirect('home')
+
+    return render(request, 'add.html')
+
+# delete_product start
+
+@login_required
+def delete_products(request,id):
+    obj = Product.objects.get(ID=id)
+    obj.delete()
+    return redirect('home')
+
+# update_product start
+
+@login_required
+def update_products(request, id):
+    obj = Product.objects.get(ID=id)
+
+    if request.method == "POST":
+        obj.name = request.POST.get('name')
+        obj.category = request.POST.get('category')
+        obj.brand = request.POST.get('brand')
+        obj.description = request.POST.get('description')
+        obj.price = request.POST.get('price')
+
+        image = request.FILES.get('image')
+        pdfs = request.FILES.get('pdfs')
+
+        if image:
+            obj.image = image
+
+        if pdfs:
+            obj.pdfs = pdfs
+
+        obj.save()  
+
+        return redirect('home')
+
+    return render(request, 'update.html', {'product': obj})
+
+# login start
+
+from django.contrib import messages
+
+# 👉 LOGIN
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid Username or Password")
+
+    return render(request, 'login.html')
+
+
+# 👉 LOGOUT
+def logout_user(request):
+    logout(request)
+    return redirect('login')
